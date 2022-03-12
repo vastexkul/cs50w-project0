5c7c1565b4b252ab64022d3cde39577e1153fc26
vastexkul <isaaclacayo08@gmail.com>
2022-03-11 20:21:26 -0600

    :lipstick: Ultimos retoques uwu - Isaac

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('#mobile-menu')
 
+const menuLinks = document.querySelector('.navbar-menu')
+
+const header = document.querySelector('.navbar')
+
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 });
 
 //This fixes the navigation bar at the top of the page
